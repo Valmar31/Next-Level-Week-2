b904a5162239f4b95a64c6d96d30966bd8501455
Valmar M M Munhoz <valmar_mmm@hotmail.com>
2020-08-08 03:03:43 -0300

    Aula 3 completa, front e banco completos

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,41 +4,54 @@ import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
 
-function TeachItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeachItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
-          alt="Diego Fernandes"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Diego Fernandes</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasata das melhores tecnologias de química avaçada.
+        {teacher.bio}
+        {/* Entusiasata das melhores tecnologias de química avaçada.
         <br />
         <br />
         Apaixonado por explodir coisas em laboratório e por mudar a vida das
         pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-        uma das minhas explosões.
+        uma das minhas explosões. */}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        {/* https://wa.me/<número> */}
+        <a href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeachItem;
