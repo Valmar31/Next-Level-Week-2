c484a21ad144456b9bf4767211249cc02605fdd0
Valmar M M Munhoz <valmar_mmm@hotmail.com>
2020-08-09 01:21:07 -0300

    1:03:55

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import Landing from "./src/pages/Landing";
 import { AppLoading } from "expo";
 
 import {
@@ -13,6 +12,7 @@ import {
   Poppins_400Regular,
   Poppins_600SemiBold,
 } from "@expo-google-fonts/poppins";
+import AppStack from "./src/routes/AppStack";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );
