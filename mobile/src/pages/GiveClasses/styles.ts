3368b0ca462ced46271436eaca6c7a667bcf1256
Valmar M M Munhoz <valmar_mmm@hotmail.com>
2020-08-09 01:25:48 -0300

    1:07:00

---
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+//Styled Components - Olhar youtube rocket
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //ocupar a tela toda
+    backgroundColor: "#8257E5",
+    justifyContent: "center",
+    padding: 40,
+  },
+});
+
+export default styles;
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Image, Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import {} from "react-native-gesture-handler";
+import { RectButton } from "react-native-gesture-handler";
 
 import styles from "./styles";
 
@@ -27,18 +27,18 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon} />
           <Text style={styles.buttonText}> Estudar</Text>
-        </TouchableOpacity>
+        </RectButton>
 
-        <TouchableOpacity
+        <RectButton
           onPress={handleNavigateToGiveClassesPage}
           style={[styles.button, styles.buttonSecondary]}
         >
           <Image source={giveClassesIcon} />
           <Text style={styles.buttonText}> Dar Aulas</Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
       <Text style={styles.totalConnections}>
