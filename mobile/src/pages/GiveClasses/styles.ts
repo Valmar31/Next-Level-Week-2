edd87ac8b70c8fe706f758f9f7aa18aadb0c9497
Valmar M M Munhoz <valmar_mmm@hotmail.com>
2020-08-09 06:04:08 -0300

    1:11:46

--- mobile/src/pages/GiveClasses/styles.ts
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -9,6 +9,28 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 40,
   },
+
+  content: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  title: {
+    fontFamily: "Archivo_700Bold",
+    color: "#fff",
+    fontSize: 32,
+    lineHeight: 37,
+    maxWidth: 180,
+  },
+
+  description: {
+    marginTop: 24,
+    color: "#D4C2FF",
+    fontSize: 16,
+    lineHeight: 26,
+    fontFamily: "Poppins_400Regular",
+    maxWidth: 240,
+  },
 });
 
 export default styles;
